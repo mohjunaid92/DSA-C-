febd1d2361a56ce9793baa2878339df4bfeed170
Moh Junaid <mohdjunaid74560@gmail.com>
2026-01-25 22:26:29 +0530

    new files

---
+++ Basics_of_OOPs.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Ammout {
+    private: 
+    int account;
+    public:
+    // constructor
+    Ammout(){
+        account =  0;
+    }
+    void balanace(int balanace){
+        account += balanace;
+    }
+    void check(){
+
+        cout<<"your balance is "<<account;
+    }
+};
+int main(){
+    Ammout s1;
+    s1.balanace(200);
+    s1.check();
+
+   
+   
+}
